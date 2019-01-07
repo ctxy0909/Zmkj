f79966547cf1f1c40c31a220498d6e082e9d0181
1360125664@qq.com <39580525+ctxy0909@users.noreply.github.com>
2019-01-07 13:42:58 +0800

    代理商登录Controller

--- Zmkj/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ Zmkj/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -272,6 +272,7 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/js/**", "anon");
         filterChainDefinitionMap.put("/ruoyi/**", "anon");
         filterChainDefinitionMap.put("/druid/**", "anon");
+        filterChainDefinitionMap.put("/reg/**", "anon");
         filterChainDefinitionMap.put("/captcha/captchaImage**", "anon");
         // 退出 logout地址，shiro去清除session
         filterChainDefinitionMap.put("/logout", "logout");
---
+++ Zmkj/src/main/java/com/ruoyi/project/system/user/controller/RegController.java
@@ -0,0 +1,24 @@
+package com.ruoyi.project.system.user.controller;
+
+import com.ruoyi.framework.web.controller.BaseController;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Auther: ct
+ * @Date: 2019/1/7 13:35
+ * @Description:
+ */
+@Controller
+@RequestMapping("/reg")
+public class RegController extends BaseController {
+
+
+    @GetMapping(value = "/toReg")
+    public String reg()
+    {
+        return "reg";
+    }
+}
